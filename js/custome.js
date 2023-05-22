5d990342cad2c02b570c5aa3c4b0b12fcff43ce0
neelgoti <67831369+neelgoti@users.noreply.github.com>
2023-05-22 18:23:04 +0530

    Add files via upload

---
+++ js/custome.js
@@ -0,0 +1,97 @@
+
+$(document).ready(function(){
+
+    $('#slider1').owlCarousel({
+        loop:true,
+        // margin:20,
+        autoplay:true,
+        autoplayTimeout:3000,
+        smartSpeed:500,
+        items:1,
+        nav:false,
+        center:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+    $('#slider2').owlCarousel({
+        loop:true,
+        margin:20,
+        autoplay:true,
+        autoplayTimeout:3000,
+        smartSpeed:500,
+        items:6,
+        nav:false,
+        center:false,
+        responsive:{
+            0:{
+                items:2
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:6
+            }
+        }
+    })
+
+    $('#slider3').owlCarousel({
+        loop:true,
+        margin:40,
+        autoplay:true,
+        autoplayTimeout:3000,
+        smartSpeed:500,
+        items:4,
+        dotsEach:true,
+        nav:false,
+        dots:true,
+        center:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+
+    $('.inner-slider01').owlCarousel({
+        loop:true,
+        margin:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        smartSpeed:500,
+        items:1,
+        dotsEach:true,
+        nav:false,
+        dots:false,
+        center:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:1
+            },
+            1000:{
+                items:1
+            }
+        }
+    })
+
+
+})
